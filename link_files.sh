040158a6df34cfa1aa3787425d2e10bcf35f190d
TimERTy <tmahonert@gmail.com>
2019-05-15 16:28:35 +1000

    added a bash script to link all of these files

---
+++ link_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOT_DIR=$(pwd)
+(
+rm -rf ~/.zshrc
+ln -s $DOT_DIR/.zshrc ~/
+
+rm -rf ~/.bashrc
+ln -s $DOT_DIR/.bashrc ~/
+
+rm -rf ~/.shrc
+ln -s $DOT_DIR/.shrc ~/
+
+rm -rf ~/.sh_aliases
+ln -s $DOT_DIR/.sh_aliases ~/
+
+rm -rf ~/.vim
+ln -s $DOT_DIR/.vim ~/
+
+rm -rf ~/.vimrc
+ln -s $DOT_DIR/.vimrc ~/
+
+rm -rf ~/.zprezto/modules/prompt/functions/prompt_timerty_setup
+ln -s $DOT_DIR/prompt_timerty_setup ~/.zprezto/modules/prompt/functions/
+)
+
